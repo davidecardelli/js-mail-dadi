88e1bd2a6b0722059afb154736cf7e4fe47474d9
davidecardelli <davide.cardelli.94@gmail.com>
2022-12-12 14:56:54 +0100

    end ex 1

--- script.js
+++ script.js
@@ -23,3 +23,32 @@ Consigli del giorno:
 4. torniamo a scrivere in italiano
 5. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"ù
 6. Il tuo codice ti sembra troppo complicato? probabilmente ti stai complicando la vita! **/
+
+// ESERCIZIO NUMERO 1:
+
+let resultPlayer1 = document.getElementById('resultPlayer1');
+let resultPlayer2 = document.getElementById('resultPlayer2');
+
+resultPlayer1 = Math.floor(Math.random() * 6 + 1);
+resultPlayer2 = Math.floor(Math.random() * 6 + 1);
+
+document.getElementById('resultPlayer1').innerText = resultPlayer1;
+document.getElementById('resultPlayer2').innerText = resultPlayer2;
+
+const player1Win = 'Il giocatore 1 ha vinto';
+const player2Win = 'Il giocatore 2 ha vinto';
+const draw = 'Parità';
+
+let finalResult = draw;
+
+if (resultPlayer1 > resultPlayer2) {
+    finalResult = player1Win
+}  
+else if (resultPlayer2 > resultPlayer1) {
+    finalResult = player2Win
+}
+
+document.getElementById('finalResult').innerText = finalResult;
+
+
+// -----------------------------------------------------------------
